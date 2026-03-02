7465456f31e558b1ec57b54ea216ce7f108a95b8
Guillaume <guillaume.robidet@outlook.fr>
2026-03-02 15:47:44 +0100

    Create a new component for header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,28 +9,7 @@
 </head>
 
 <body>
-    <header class="home">
-        <div class="left"></div>
-
-        <div class="center">
-            <a href="{{ route('home') }}">
-                <img src="{{ asset('images/logo-aventura-o3.png') }}" alt="Logo">
-            </a>
-        </div>
-
-        <div class="right">
-            <img src="{{ asset('images/menu-white.png') }}" alt="Menu" class="burger">
-            <img src="{{ asset('images/close.png') }}" alt="Close" class="burger-close">
-        </div>
-
-        <nav class="nav">
-            <ul>
-                <li><a href="{{ route('home') }}" class="menu-link">Accueil</a></li>
-                <li><a href="#" class="menu-link">Nos créations</a></li>
-                <li><a href="#" class="menu-link">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+    <x-header></x-header>
 
     <!-- OVERLAY -->
     <div class="menu-overlay"></div>
