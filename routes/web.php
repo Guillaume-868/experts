1903e32316650485054b2db60dcae3ed746d9193
Guillaume <guillaume.robidet@outlook.fr>
2026-03-02 16:18:42 +0100

    First commit

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 })->name('home');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
